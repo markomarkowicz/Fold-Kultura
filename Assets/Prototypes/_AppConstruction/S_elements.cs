39d7689cabf31ead4710f151367f0a8e112e87fb
jatczyn <jatczynski@gmail.com>
2018-02-11 04:25:47 +0100

    add AppConstructionPrototype

---
+++ Assets/Prototypes/_AppConstruction/S_elements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_elements : MonoBehaviour {
+	//public int typesToys=7;
+	//public GameObject body;
+	public GameObject[] addons;
+	public Texture[] colorToys;
+	//public GameObject[][3] tex
+
+	// Use this for initialization
+	void Start () {
+	//	addons= new GameObject[7];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
