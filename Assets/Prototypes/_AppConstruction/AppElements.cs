f5ff3337727d95fb4710d8ace269bd0ab0ca4a2a
jatczyn <jatczynski@gmail.com>
2018-02-27 17:40:38 +0100

    modify global preafb scripts

--- Assets/Prototypes/_AppConstruction/AppElements.cs
+++ Assets/Prototypes/_AppConstruction/AppElements.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AppElements : MonoBehaviour {
 	public GameObject[] sList;
+
 	// Use this for initialization
 	public GameObject testerD;
 
@@ -12,17 +13,46 @@ public class AppElements : MonoBehaviour {
 	//GameObject d = sList [2].GetComponent<S_elements> ();
 
 		 
-		testerD = sList [1].GetComponent<S_elements> ().addons[2];
+	//	testerD = sList [2].GetComponent<S_elements> ().addons[1];
 
-		Instantiate (testerD, transform.position, transform.rotation);
+	//	Instantiate (testerD, transform.position, transform.rotation);
 
 
 	//	testerD = Instantiate (d.addons[2], transform.position, transform.rotation);
 
 	}
+
+
+	void WrzucToysa (GameObject toyParent, GameObject[] toySet,int tb,int toyadd,int toyMaterial){
+		GameObject toyBase, toyAddon, toyColor;
+
+
+		toyBase = toySet[tb].GetComponent<S_elements> ().baseModel;
+		toyMaterial = toySet [tb].GetComponent<S_elements> ().matToys;
+		Instantiate (toyBase, testerD.transform.position, testerD.transform.rotation, testerD.transform);
+		toyMaterial = GetComponentInChildren<Material> ();
+
+		toyBase.transform.localPosition = Vector3.zero;
+
+		toyAddon = toySet [tb].GetComponent<S_elements> ().addons [toyadd];
+		Instantiate (toyAddon, testerD.transform.position, testerD.transform.rotation, testerD.transform);	
+
+
+
+
+
 	
+	}
+
+
+
+
+
+
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown("k")){
+			WrzucToysa (testerD, sList, 2, 2, 1);
+		}
 	}
-}
+}	
--- Assets/Prototypes/_AppConstruction/S_elements.cs
+++ Assets/Prototypes/_AppConstruction/S_elements.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class S_elements : MonoBehaviour {
 	//public int typesToys=7;
 	//public GameObject body;
+	public GameObject baseModel;
 	public GameObject[] addons;
-	public Texture[] colorToys;
+	public Material[] matToys;
 	//public GameObject[][3] tex
 
 	// Use this for initialization
