d76bfe18c8c1fb55568e85cd551ebac52dcdc1bc
Jatczyn <jatczynski@gmail.com>
2018-02-27 22:42:39 +0100

    add prefab scripts toy with spawner prototype

---
+++ Assets/Prototypes/ToyElements.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyElements : MonoBehaviour {
+
+	public GameObject toyBase;
+	GameObject toyBaseR;
+	public GameObject toyAddon;
+	public Material[] toyMaterila;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+
+	public void ToyOn(int colMat,GameObject toyParent){
+		Instantiate (toyBase, toyParent.transform.position, toyParent.transform.rotation, toyParent.transform);
+		Instantiate (toyAddon, toyParent.transform.position, toyParent.transform.rotation, toyParent.transform);
+		toyBase.GetComponentInChildren<Renderer> ().material = toyMaterila [colMat];
+		toyAddon.GetComponentInChildren<Renderer> ().material = toyMaterila [colMat];
+	
+	
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Prototypes/_AppConstruction/AppElements.cs
+++ Assets/Prototypes/_AppConstruction/AppElements.cs
@@ -7,7 +7,7 @@ public class AppElements : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject testerD;
-
+	public Renderer[] toyRenderer;
 
 	void Start () {
 	//GameObject d = sList [2].GetComponent<S_elements> ();
@@ -21,14 +21,19 @@ public class AppElements : MonoBehaviour {
 	//	testerD = Instantiate (d.addons[2], transform.position, transform.rotation);
 
 	}
-
-
-	void WrzucToysa (GameObject toyParent, GameObject[] toySet,int tb,int toyadd,int toyMaterial){
+		void WrzucToysa (GameObject toyParent, GameObject[] toySet,int tb,int toyadd,int toyMaterial){
 		GameObject toyBase, toyAddon, toyColor;
-
-
+		Material mati;
+		// LATER ////// toy material id [0-2] [color,lineart,flat] mnozony razy addon podobnie moze być dobierana baza toysa, 
 		toyBase = toySet[tb].GetComponent<S_elements> ().baseModel;
-	//	toyMaterial = toySet [tb].GetComponent<S_elements> ().matToys;
+	//	
+
+	//	mati = toySet [tb].GetComponents<S_elements> ().matToy [2];
+	//		toyRenderer [1] =  toySet[tb].GetComponentsInChildren<Renderer> ();
+
+	//	toyRenderer [1].material = matToy [2]; 
+
+//			toyMaterial = toySet [tb].GetComponent<S_elements> ().matToys;
 		Instantiate (toyBase, testerD.transform.position, testerD.transform.rotation, testerD.transform);
 	//	toyMaterial = GetComponentInChildren<Material> ();
 	//	toyMaterial = 
