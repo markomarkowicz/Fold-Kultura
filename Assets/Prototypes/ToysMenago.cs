8be5c10ae8a9412abe671ff1daeefe140d8c037b
Jatczyn <jatczynski@gmail.com>
2018-02-28 21:37:00 +0100

    add script

---
+++ Assets/Prototypes/ToysMenago.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToysMenago : MonoBehaviour {
+
+	public ToyElements[] toysSC;
+	GameObject[] toysSCRun;
+	public GameObject[] paripati;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		toysSC [1].ToyOn (0,paripati[0]);
+		toysSC [0].ToyOn (2,paripati[1]);
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
